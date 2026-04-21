0927e5821f017ac8616baaccf9f695ad0683ef37
Тимофей Яровик <timofey2009yarovik@gmail.com>
2026-04-21 23:45:43 +0300

    feat: establish project structure and core models for booking system

--- project_backend/main_settings/urls.py
+++ project_backend/main_settings/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for my_tg_bot project.
+URL configuration for main_settings project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
